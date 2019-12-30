ca9dfc074c0a52c773fabd7ee3af2f81b037c8ed
Sami Shakir <sami.shakir@hostopia.com.au>
2019-12-30 13:02:06 +1100

    auto commit from sami-desktop.dpac.com.au

---
+++ bash/README.md
@@ -0,0 +1,6 @@
+# Spam Checker - How to use
+
+touch ~/chk_spam.sh; chmod +x ~/chk_spam.sh; vim ~/chk_spam.sh
+-- Copy Paste the script --
+Fill in the MSGID as param 1 (below) when calling script.
+~/chk_spam.sh 1ilXEq-003lzk-FR
--- bash/spam_checker.sh
+++ bash/spam_checker.sh
@@ -1,4 +1,6 @@
-MSGID="1ijEtN-00E7fj-AC";
+#!/bin/bash
+# Sami S - Hostopia AU 2019
+MSGID="$1";
 if [[ -z $MSGID ]]; then
     INVESTIGATE=$(exim -bp | awk -F '<' '/^ *[0-9]+/{print $2}' | cut -d '>' -f1 | sort | uniq -c | sort -n | grep -v $(facter fqdn) | tail -n 1 | awk '{print $2}')
     MSGID=$(exim -bp | grep -B 1 ${INVESTIGATE} | grep \< | awk '{print $3}' | tail -n 1)
@@ -16,32 +18,37 @@ else
         MSG=$(zgrep ${MSGID} /var/log/exim_mainlog*)
     fi
 fi
-
 DOVEMETHOD=$(echo "${MSG}" | grep -oP 'dovecot_(plain|login)');
-USRNAME=$(echo "${MSG}" | grep 'U=' | awk -F 'U=' '{print $2}' | grep -v mailnull | awk '{print $1}' | head -n 1);
-if [[ -z $USRNAME ]]; then
-    USRNAME=$(echo "${MSG}" | grep "SpamAssassin as" | awk -F 'SpamAssassin as' '{print $2}' | awk '{print $1}' | head -n 1)
-else
-    USRNAME="Cannot determine cpanel username!"
-fi
+DOVERETURN=$(echo "${MSG}" | awk -v dove="$DOVEMETHOD" -F $DOVEMETHOD: '/dove/ {print $2}' | awk '{print $1}');
+SUBJECT=$(echo "${MSG}" | grep "T=" | head -n 1 | grep -oP "T=\".*\"" | cut -d '"' -f 2);
 MSGIP=$(echo "${MSG}" | grep "H=" | head -n 1 | grep -oP "(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))" | tail -n 1)
 RETMSGIP=$?
+USRNAME=$(echo "${MSG}" | grep 'U=' | awk -F 'U=' '{print $2}' | grep -v mailnull | awk '{print $1}' | head -n 1);
+if [[ -z "${USRNAME}" ]]; then
+    USRNAME=$(echo "${MSG}" | grep "SpamAssassin as" | awk -F 'SpamAssassin as' '{print $2}' | awk '{print $1}' | head -n 1)
+fi
+if [[ -z "${USRNAME}" ]]; then
+    USRNAME=$(grep "${DOMAIN}" /etc/trueuserdomains | awk -F ':' '{print substr($2,2)}')
+fi
+DOMAIN=$(echo ${DOVERETURN} | awk -F '@' '{print $2}');
+if [[ -z "${DOMAIN}" ]]; then
+    DOMAIN=$(grep ${USRNAME} /etc/trueuserdomains | awk -F: '{print $1}')
+fi
+echo -e "\nFull Logs:\n\n-------------------------------------------------------------------------------\n\nDid this return the right logs? (note it could be a bounceback)\n\n${MSG}\n\n-------------------------------------------------------------------------------\n"
+echo -e "Cpanel username of the originating email is:\t${USRNAME}";
+echo -e "Domain for this account is: \t\t\t${DOMAIN}";
+if [[ ${RETMSGIP} -eq 0 ]]; then
+    echo -e "Connecting IP sending to exim:\t\t\t${MSGIP}"
+    echo -e "Geoiplookup of this IP:\t\t\t\t$(geoiplookup ${MSGIP})\n"
+fi
 if [[ -n ${DOVEMETHOD} ]]; then
-    DOVERETURN=$(echo "${MSG}" | awk -v dove="$DOVEMETHOD" -F $DOVEMETHOD: '/dove/ {print $2}' | awk '{print $1}')
-    SUBJECT=$(echo "${MSG}" | grep "T=" | head -n 1 | grep -oP "T=\".*\"" | cut -d '"' -f 2)
-    echo -e "\nFull Logs:\n\n-------------------------------------------------------------------------------\n\n${MSG}\n\n-------------------------------------------------------------------------------\n"
     echo -e "Appears to be dovecot, the sending email is: ${DOVERETURN}"
-    if [[ ${RETMSGIP} -eq 0 ]]; then
-        echo -e "\nThe connecting IP was:\t\t${MSGIP}"
-        echo -e "Geoiplookup of this IP:\t\t$(geoiplookup ${MSGIP})"
-    fi
     echo -e "The Subject of the email was:\t${SUBJECT}\n"
     echo "Use this command to roll the password (only if you know it's compromised):"
     echo -e "/usr/local/cpanel/bin/uapi --user=${USRNAME} Email passwd_pop email=$(echo ${DOVERETURN} | awk -F '@' '{print $1}') password=$(openssl rand -base64 15) domain=$(echo ${DOVERETURN} | awk -F '@' '{print $2}')\n"
     echo -e "Send this to au-servicedesk-alerts:\nCan someone please get in touch with '"${USRNAME}"' on '$(facter fqdn)'\nCompromised email password has been rolled -> ${DOVERETURN}\n"
 else
-    echo -e "\nDid this return the right logs? (note it could be a bounceback)\n\n${MSG}"
-    echo -e "\nIt's not dovecot, Checking for compromised scripts...\n"
+    echo -e "It's not dovecot, Checking for compromised scripts...\n"
     SCRIPTSITES=$(grep cwd /var/log/exim_mainlog | grep -v /var/spool | awk -F"cwd=" '{print $2}' | grep -vP ^$ | awk '{print $1}' | sort | uniq -c | sort -n | grep -v '\/tmp\/\|\/usr\/local\|\/etc\/csf\|\/root\|\/$' | tail)
     echo -e "${SCRIPTSITES}"
     echo "${SCRIPTSITES}" | while read line; do
@@ -50,15 +57,20 @@ else
         if [[ -n ${COMCONTACT} ]]; then
             echo -e "\nThe following account has a potential joomla compromise:\n------------------------------"
             echo -e "Username ${HOMEDIR}:\n${COMCONTACT}";
-            echo -e "\nEdit the htaccess with:\nvim $(echo ${line} | awk '{print $2}')/.htaccess"
+            echo -e "\nEdit the htaccess with:\nvim $(echo ${line} | awk '{print $2}')/.htaccess\n"
+            cat << EOF
+Put this in the htaccess:
+
+RewriteCond %{QUERY_STRING} !^/?option=
+RewriteRule .? - [S=2]
+RewriteCond %{QUERY_STRING} !/?option=([a-z0-9_]+)&view=.* [NC]
+RewriteRule .? - [R=403]
+RewriteCond %{QUERY_STRING} /?option=com_contact [NC]
+RewriteRule .? - [R=403]
+EOF
             echo -e "\nSend this to au-servicedesk-alerts:\n\nCan someone get in touch with '"${HOMEDIR}"' on '$(facter fqdn)'\nCompromised joomla form on website '$(grep "${HOMEDIR}" /etc/trueuserdomains | awk -F: '{print $1}')' blocked in htaccess\nReseller Owner account name is '$(grep OWNER /var/cpanel/users/${HOMEDIR} | awk -F= '{print $2}')'\n------------------------------"
         fi
     done
-    echo -e "\nCpanel username of the originating email is:\t${USRNAME}";
-    echo -e "Domain for this account is: \t\t\t$(grep ${USRNAME} /etc/trueuserdomains | awk -F: '{print $1}')";
-    if [[ ${RETMSGIP} -eq 0 ]]; then
-        echo -e "Connecting IP sending to exim:\t\t\t${MSGIP}"
-        echo -e "Geoiplookup of this IP:\t\t\t\t$(geoiplookup ${MSGIP})\n"
-        echo -e "sample of script sending logs from the IP:\n$(grep ${MSGIP} /home/${USRNAME}/access-logs/* 2> /dev/null | tail)\n"
-    fi
+    # echo -e "sample of script sending logs from the IP:\n$(grep ${MSGIP} /home/${USRNAME}/access-logs/* 2> /dev/null | tail)\n"
+    echo -e "\nSend this to au-servicedesk-alerts:\n\nCan someone get in touch with '"${USRNAME}"' on '$(facter fqdn)'\nCompromised form on website '${DOMAIN}' is sending spam - blocked in htaccess\n"
 fi
