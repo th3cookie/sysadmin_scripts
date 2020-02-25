2d99a52ba2195ff508488a24a5a40ab2fdd945c5
Sami Shakir <sami.shakir@hostopia.com.au>
2020-02-25 17:10:53 +1100

    auto commit from $(hostname)

--- python/cpanel-config.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3.7
-
-# Steps #
-# Have pupept file manage the config backup tar
-# If the cpanel.config file differs from the tar that puppet manages (./cpanel/system/whmconf/config/cpanel.config), 
