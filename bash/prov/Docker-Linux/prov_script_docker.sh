07938fac2ed46acd241b27af4f7f6dc693baee77
th3cookie <th3cookie@gmail.com>
2020-03-07 20:45:57 +1100

    auto commit from Sami-Win10-PC

--- bash/prov/Docker-Linux/prov_script_docker.sh
+++ bash/prov/Docker-Linux/prov_script_docker.sh
@@ -115,6 +115,7 @@ if [[ -z ${PY_VERSION} ]]; then
 fi
 pip3 install -U pip
 pip3 install pyyaml
+pip3 install -U pyyaml
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 $INSTALL_COMMAND update
