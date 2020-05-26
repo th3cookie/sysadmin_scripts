5a196612c7961bb0e8d58f920344f8b4f7007a3c
th3cookie <th3cookie@gmail.com>
2020-05-26 14:02:02 +1000

    auto commit from Sami-Win10-PC

--- bash/work/load_investigations.sh
+++ bash/work/load_investigations.sh
@@ -2,7 +2,9 @@
 for i in {1..20}; do ps aux | grep php | awk '{print $1}' | sort | uniq -c | sort -n | awk '{print $2}' | tail >> ~/investigation.log; sleep 1; done; cat ~/investigation.log | sort | uniq -c | sort -n
 
 # Most apache access log hits at a certain time, sorted (adjust search time variable)
-SEARCH="29/Apr/2020:09:[1-2][0-9]:"
+SEARCH="26/May/2020:13:1[0-4]:"
+# Current minute in real-time
+SEARCH=$(date +%d/%b/%Y:%H:%M:)
 for USER in $(awk -F\  '{print $2}' /etc/trueuserdomains); do echo "$(grep ${SEARCH} /home/$USER/access-logs/* 2> /dev/null | wc -l) - ${USER}"; done | sort -n
 
 # Then to find the most endpoint hits at that time (update USER and SEARCH (above) variable with username and timestamp to search through logs)
