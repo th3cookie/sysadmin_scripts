703f86a6ac787f70ed08b7139b98cebc560dfa0c
th3cookie <th3cookie@gmail.com>
2020-05-27 20:27:20 +1000

    auto commit from Sami-Win10-PC

--- work/others/cloudflare_panthur/cloudflare_setup.py
+++ work/others/cloudflare_panthur/cloudflare_setup.py
@@ -5,35 +5,43 @@ import json
 import requests
 import config
 
+if __name__=="__main__":
+    # Load API Key from config.py
+    api_key = config.api_key
 
-email = input("Client Email: ")
-password = input("Password: ")
-domain = input("Domain: ")
-id = domain.replace('.', '')
-url = "https://api.cloudflare.com/host-gw.html"
+    # Read user input
+    email = input("Client Email: ")
+    password = input("Password: ")
+    domain = input("Domain: ")
 
-# Test the ID is not in use first
-print(f"Testing if the ID \"{id}\" is in use before proceeding...")
-user_lookup_data = {'act': 'user_lookup', 'host_key': api_key, 'unique_id': id}
-response = requests.post(url, data = user_lookup_data)
+    # Make a unique ID by stripping dots from domain name (but we will do checks for this anyway)
+    id = domain.replace('.', '')
 
-print(response)
+    # Cloudflare API endpoint URL
+    url = "https://api.cloudflare.com/host-gw.html"
 
+    # Test the ID is not in use first
+    print(f"Testing if the ID \"{id}\" is in use before proceeding...")
+    user_lookup_data = {'act': 'user_lookup', 'host_key': api_key, 'unique_id': id}
+    response = requests.post(url, data = user_lookup_data)
 
-'''
-# Create user in partner account
-user_create_data = {'act': 'user_create', 'host_key': api_key, 'cloudflare_email': email, 'cloudflare_pass': password, 'unique_id': id}
-
-
-
-USER_KEY=
-
-grab user_key
+    print(response)
 
 
-curl https://api.cloudflare.com/host-gw.html \
--d "act=full_zone_set" \
--d "host_key=${API_KEY}" \
--d "user_key=CLIENT_KEY_FROM_ABOVE_CALL" \
--d "zone_name=${DOMAIN}"
-'''
+    '''
+    # Create user in partner account
+    user_create_data = {'act': 'user_create', 'host_key': api_key, 'cloudflare_email': email, 'cloudflare_pass': password, 'unique_id': id}
+
+
+
+    USER_KEY=
+
+    grab user_key
+
+
+    curl https://api.cloudflare.com/host-gw.html \
+    -d "act=full_zone_set" \
+    -d "host_key=${API_KEY}" \
+    -d "user_key=CLIENT_KEY_FROM_ABOVE_CALL" \
+    -d "zone_name=${DOMAIN}"
+    '''
