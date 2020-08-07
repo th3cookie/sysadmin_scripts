c12f82765aa697060ccd6ce498061f426cb17249
th3cookie <th3cookie@gmail.com>
2020-08-07 22:14:50 +1000

    auto commit from Sami-Win10-PC

--- personal/homelab_scripts/update_cf_ips/update_cf_ip.py
+++ personal/homelab_scripts/update_cf_ips/update_cf_ip.py
@@ -25,8 +25,26 @@ if __name__=="__main__":
 	# Cloudflare API endpoint URL
 	url = "https://api.cloudflare.com/client/v4/zones/"
 
+	# Get Public IP
 	proc = subprocess.run(
-		['ls', '-l'],
+		['dig', '+short', 'myip.opendns.com', '@resolver1.opendns.com'],
 		stdout=subprocess.PIPE,
 		stderr=subprocess.PIPE,
+		universal_newlines=True,
 	)
+
+	ip = proc.stdout.strip()
+	print(ip)
+	
+	'''
+	Example adding DNS record:
+
+	curl -X PUT "https://api.cloudflare.com/client/v4/zones/023e105f4ecef8ad9ca31a8372d0c353/dns_records/372e67954025e0ba6aaa6d586b9e0b59" \
+     -H "X-Auth-Email: user@example.com" \
+     -H "X-Auth-Key: XXXXXXXXXXXXXXXXXXXXX" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"example.com","content":"127.0.0.1","ttl":{},"proxied":false}'
+
+	dns records to do:
+	home
+	 '''
